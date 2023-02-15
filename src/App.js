26db9e3b67186126ecc5cbd8361ec27baadfe8e3
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-02-15 18:49:28 -0300

    exercicios-concluidos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { ChakraProvider, Flex } from '@chakra-ui/react'
+import Card from './components/Card'
 
 export default function App() {
   const [users, setUsers] = useState([]);
+
+  console.log(users)
 
   useEffect(() => {
     fetchUsers();
@@ -22,9 +26,21 @@ export default function App() {
   };
 
   return (
-    <>
-      <h1>Me apague quando for iniciar!</h1>
-      <p>Chame o Card aqui!</p>
-    </>
+    <ChakraProvider resetCSS>
+      <Flex gap={'16px'} wrap={'wrap'} justifyContent={'center'}>
+        {users && users.map((user) => {
+          return <Card
+            key={user.id}
+            user={user.name}
+            site={user.website}
+          />
+        })}
+
+      </Flex>
+
+
+    </ChakraProvider>
+
+
   );
 }
